1ee26b3b4dd2ec00fea425e44952cd7efcf2569e
Drumsid <den-sidnay@yandex.ru>
2019-10-09 22:14:05 +0300

    add work even game

--- src/games/evenGames.php
+++ src/games/evenGames.php
@@ -1,57 +1,57 @@
-<?php 
+<?php
 
 namespace BrainGames\evenGames;
 
 use function cli\line;
+
 use function cli\prompt;
-
-
 
 // генерирует число и проверяет положительное оно или нет, вернет массив [num, bool]
 function generateNum()
 {
-	$result = [];
-	$num = rand(1, 100);
-	$bool = isEven($num);
-	$result['number'] = $num;
-	$result['even'] = $bool;
-	return $result;
+    $result = [];
+    $num = rand(1, 100);
+    $bool = isEven($num);
+    $result['number'] = $num;
+    $result['even'] = $bool;
+    return $result;
 }
 
 // проверяем положительное ли число
 function isEven($num)
 {
-	if ($num % 2 == 0)
-	{
-		return 'yes';
-	}
-	return 'no';
+    if ($num % 2 == 0) {
+        return 'yes';
+    }
+    return 'no';
 }
 
 // задаем вопрос пользователю
 function askQuestion($arr)
 {
-	line("Question: {$arr['number']}");
-	$answer = prompt("Your answer");
-	return $answer;
+    line("Question: {$arr['number']}");
+    $answer = prompt("Your answer");
+    return $answer;
 }
 
 // корректный ли ответ?
 function isCorrectAnswer($answer, $evenOrNot, $name)
 {
-	if ($answer !== 'yes' && $answer !== 'no') {
+    if ($answer !== 'yes' && $answer !== 'no') {
+        line("The answer can be only 'yes' or 'no'. Let's try again, {$name}!");
+        return 0;
+    } elseif (($answer === 'yes' && $evenOrNot === 'yes') || ($answer === 'no' && $evenOrNot === 'no')) {
+        line("Correct!");
+        return 1;
+    } else {
+        line("{$answer} is wrong answer ;(. Correct answer was {$evenOrNot}. Let's try again, {$name}!");
+        return 0;
+    }
+}
 
-		line("The answer can be only 'yes' or 'no'");
-
-	}
-	else if (($answer === 'yes' && $evenOrNot === 'yes') || ($answer === 'no' && $evenOrNot === 'no')) {
-
-		line("Correct!");
-
-	}
-	else {
-
-		line("{$answer} is wrong answer ;(. Correct answer was {$evenOrNot}. Let's try again, {$name}!");
-
-	}
+// Congratulations
+function myCongratz($name)
+{
+    line("Congratulations, %s!", $name);
+    die;
 }
